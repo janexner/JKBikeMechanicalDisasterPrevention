4f5dbc4a3bf7951b3b3f5684231d7a91b0432cb4
jexner <jan.exner@gmx.net>
2025-02-22 23:14:07 +0100

    in which our hero translates

--- app/src/main/java/com/exner/tools/jkbikemechanicaldisasterprevention/ui/destinations/About.kt
+++ app/src/main/java/com/exner/tools/jkbikemechanicaldisasterprevention/ui/destinations/About.kt
@@ -41,15 +41,15 @@ fun About(
             PageHeaderTextWithSpacer(stringResource(R.string.about))
             Text(text = "Version " + BuildConfig.VERSION_NAME)
             DefaultSpacer()
-            Text(text = "J-K Bike - Mechanical Disaster Prevention is a free, open source app that helps build positive bike maintenance behaviours, for rides quick, big or epic.")
+            Text(text = stringResource(R.string.about1))
             DefaultSpacer()
-            Text(text = "We have all experienced those rides where something mechanical happens that really was preventable.")
+            Text(text = stringResource(R.string.about2))
             DefaultSpacer()
-            Text(text = "As the authors, even though we take a certain pride in our own efforts in maintaining smooth running MTBs, and a disdain for our riding buddies who never give a passing thought until something goes 'CRUNCH!' in a ride-ending manner, there are still innumerate situations where something that should have been checked, or usually never needs checked after proper installation, has not been.")
+            Text(text = stringResource(R.string.about3))
             DefaultSpacer()
-            Text(text = "'Where is that vibration coming from?', 'why is the rear end creaking?', 'What is that terrible knocking?' are so often result of something relatively simple being overlooked or forgotten. Recent favourites have been disc rotor bolts backing out, rear thru axles not done up properly, and headsets improperly tensioned.")
+            Text(text = stringResource(R.string.about4))
             DefaultSpacer()
-            Text(text = "So, to prevent mechanical disasters happening on your rides, use this app to build better, new, habits and ensure your rides are (mechanically) glorious.")
+            Text(text = stringResource(R.string.about5))
         }
     }
 }
