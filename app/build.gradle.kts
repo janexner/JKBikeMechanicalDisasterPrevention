43e5a2da0fc41d1892d2c0cac4f5dd73d3257f83
jexner <jan.exner@gmx.net>
2025-02-25 13:17:50 +0100

    in which our hero updates versions and notes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 30
         targetSdk = 35
         versionCode = 6
-        versionName = "1.3.1"
+        versionName = "1.4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
