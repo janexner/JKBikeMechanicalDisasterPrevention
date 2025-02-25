1cd328395ba7ba26ac4c4a3794aaede9a7730141
jexner <jan.exner@gmx.net>
2025-02-25 21:45:02 +0100

    in which our hero deletes a little more

--- app/src/main/java/com/exner/tools/jkbikemechanicaldisasterprevention/state/SelectedBikeState.kt
+++
@@ -1,7 +0,0 @@
-package com.exner.tools.jkbikemechanicaldisasterprevention.state
-
-import com.exner.tools.jkbikemechanicaldisasterprevention.database.entities.Bike
-
-data class SelectedBikeState(
-    val selectedBike: Bike? = null
-)
